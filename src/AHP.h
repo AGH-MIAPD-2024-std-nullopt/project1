3a54525909071145e101a73fcc78b996fcbdce20
Maciej Grzybacz <128727060+MaciejGrzybacz@users.noreply.github.com>
2024-11-15 13:53:08 +0100

    simple ahp implementation

--- src/AHP.h
+++ src/AHP.h
@@ -1,39 +1,49 @@
 #pragma once
-#include <vector>
-#include <string>
-#include <optional>
+
 #include <map>
+#include <optional>
+#include <string>
+#include <vector>
 
 #include <Eigen/Dense>
 
-// This is only a stub for the actual implementation
-// Details are yet to be determined later
-
-class AHPRanker 
-{
-  using Matrix2D = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+class AHPRanker {
 public:
-  AHPRanker();
-  AHPRanker(AHPRanker&&) = default;
-  AHPRanker(const AHPRanker&) = default;
+    using Matrix2D = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
 
-  void addComparison(const std::string& alternative1, 
-                      const std::string& alternative2, 
-                      const std::string& criteria, 
-                      double value);
-  void calculate();
+    AHPRanker() = default;
+    AHPRanker(const AHPRanker&) = default;
+    AHPRanker(AHPRanker&&) = default;
+    AHPRanker& operator=(const AHPRanker&) = default;
+    AHPRanker& operator=(AHPRanker&&) = default;
 
-  std::vector<double> getRanking();
-  double getConsistencyRatio();
+    void addComparison(const std::string& alternative1,
+                       const std::string& alternative2,
+                       const std::string& criteria,
+                       double value);
 
-protected:
-  void buildMatrix();
+    void readSingleAgentCsv(const std::string& filename);
+    void readMultiAgentsCsv(const std::vector<std::string>& filenames);
 
-  using ComparisonMap = std::map<std::pair<std::string, std::string>, double>;    // key: (alternative1, alternative2), value: value of this comparison
-  using ComparisonsByCriteriaMap = std::map<std::string, ComparisonMap>;          // key: criteria name, value: ComparisonMap for this criteria
-  ComparisonsByCriteriaMap comparisons = {{}};
+    void calculate();
 
-  std::optional<Matrix2D> matrix                          = std::nullopt;
-  std::optional<std::vector<size_t>> calculatedRanking    = std::nullopt;
-  std::optional<double> calculatedConsistencyRatio        = std::nullopt;
+    std::vector<double> getRanking();
+    std::vector<std::string> getCriteria();
+    double getConsistencyRatio();
+
+private:
+    Matrix2D buildAvgMatrix(const std::vector<Matrix2D>& matrices);
+    
+    std::vector<double> calculateRanking();
+    double calculateConsistencyRatio();
+    double getRandomConsistencyIndex(size_t n);
+
+    using ComparisonMap = std::map<std::pair<std::string, std::string>, double>;
+    using ComparisonsByCriteriaMap = std::map<std::string, ComparisonMap>;
+    ComparisonsByCriteriaMap comparisons;
+
+    std::optional<std::vector<std::string>> criteria;
+    std::optional<Matrix2D> comparison_matrix;
+    std::optional<std::vector<double>> calculatedRanking;
+    std::optional<double> calculatedConsistencyRatio;
 };
