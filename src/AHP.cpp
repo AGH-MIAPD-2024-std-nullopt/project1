55bb2c979a357a3508cc6b784dfc8a7db179cbb7
Maciej Grzybacz <128727060+MaciejGrzybacz@users.noreply.github.com>
2024-12-10 01:15:42 +0100

    corrected AHP

--- src/AHP.cpp
+++ src/AHP.cpp
@@ -1,9 +1,93 @@
 #include "AHP.h"
-
-#include <fmt/core.h>
-#include <fstream>
 #include <numeric>
+#include <cmath>
 
-using namespace AHP;
+AHP::WeightsAndIR AHP::AHPRanker::calculateWeightsAndIR(AHP::Matrix2D matrix)
+{
+    const size_t n = matrix.rows();
+    Eigen::VectorXd weights = Eigen::VectorXd::Zero(n);
 
-//TODO
+    for (size_t i = 0; i < n; i++) {
+        double product = 1.0;
+        for (size_t j = 0; j < n; j++) {
+            product *= matrix(i, j);
+        }
+        weights(i) = std::pow(product, 1.0/n);
+    }
+
+    weights /= weights.sum();
+
+    double lambda_max = 0.0;
+    for (size_t i = 0; i < n; i++) {
+        double sum_row = 0.0;
+        for (size_t j = 0; j < n; j++) {
+            sum_row += matrix(i, j) * weights(j);
+        }
+        lambda_max += sum_row / weights(i);
+    }
+    lambda_max /= n;
+
+    double CI = (lambda_max - n) / (n - 1);
+
+    const std::vector<double> RI = {0.0, 0.0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49};
+
+    double CR = 0.0;
+    if (n > 1 && n <= 10) {
+        CR = CI / RI[n-1];
+    }
+
+    std::vector<double> weights_std(weights.data(), weights.data() + weights.size());
+
+    return {weights_std, CR};
+}
+
+std::vector<double> AHP::AHPRanker::calculateRankingVector(AHP::Matrix2D matrix) {
+    const int alternatives = matrix.rows();
+    const int criteria = matrix.cols();
+
+    Eigen::VectorXd criteriaWeights = matrix.row(0);
+
+    std::vector<double> ranking(alternatives - 1, 0.0);
+
+    for(int i = 1; i < alternatives; i++) {
+        double sum = 0.0;
+        for(int j = 0; j < criteria; j++) {
+            sum += matrix(i, j) * criteriaWeights(j);
+        }
+        ranking[i-1] = sum;
+    }
+
+    return ranking;
+}
+
+AHP::AHPResult
+AHP::AHPRanker::calculateRanking(AHP::Matrix2D criteria_comparison, std::vector<Matrix2D> alternatives_comparisons) {
+    auto [criteria_weights, criteria_ir] = calculateWeightsAndIR(criteria_comparison);
+
+    const size_t n_criteria = criteria_weights.size();
+    const size_t n_alternatives = alternatives_comparisons[0].rows();
+    Matrix2D ranking_matrix(n_alternatives + 1, n_criteria);
+
+    for (size_t i = 0; i < n_criteria; i++) {
+        ranking_matrix(0, i) = criteria_weights[i];
+    }
+
+    std::vector<double> inconsistency_ratios;
+    for (size_t i = 0; i < n_criteria; i++) {
+        auto [weights, ir] = calculateWeightsAndIR(alternatives_comparisons[i]);
+        inconsistency_ratios.push_back(ir);
+
+        for (size_t j = 0; j < n_alternatives; j++) {
+            ranking_matrix(j + 1, i) = weights[j];
+        }
+    }
+
+    std::vector<double> final_ranking = calculateRankingVector(ranking_matrix);
+
+    AHPResult result;
+    result.ranking = final_ranking;
+    result.criteriaIRatio = criteria_ir;
+    result.alternativesIRatios = inconsistency_ratios;
+
+    return result;
+}
--- src/AHP.h
+++ src/AHP.h
@@ -3,40 +3,50 @@
 #include <map>
 #include <optional>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <Eigen/Dense>
 
 namespace AHP {
-  using Matrix2D = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using Matrix2D = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using ComparisonValues = std::map<std::string, double>; // alt2 -> value
+    using Comparisons = std::map<std::string, ComparisonValues>; // alt1 -> map of comaprison values for each alt2
+    using WeightsAndIR = std::pair<std::vector<double>, double>; // weights and inconsistancy ratio for a single matrix
 
-  using ComparisonValues = std::map<std::string, double>; // alt2 -> value
-  using Comparisons = std::map<std::string, ComparisonValues>; // alt1 -> map of comaprison values for each alt2
+    struct AHPResult {
+        std::vector<double> ranking;             // Final ranking of alternatives
+        double criteriaIRatio;           // Inconsistency ratio for criteria comparison matrix
+        std::vector<double> alternativesIRatios;  // Inconsistency ratios for comparison of alternatives by single criterium 
+    };
+    
+    struct AgentInput {
+        std::map<std::string, Comparisons> altComparisons;  // criteria -> comparisons of alternatives in this criteria
+        Comparisons critComparisons;
+    };
 
-  struct AgentInput {
-    std::map<std::string, Comparisons> altComparisons;  // criteria -> comparisons of alternatives in this criteria
-    Comparisons critComparisons;
-  };
+    class AHPMatrixBuilder {
+    public:
+        AHPMatrixBuilder(const std::vector<std::string>& alternatives, const std::vector<std::string>& criteria);
 
+        void addComparison(const std::string& alt1, const std::string& alt2, double val);
 
-  class AHPMatrixBuilder {
-  public:
-    AHPMatrixBuilder(const std::vector<std::string>& alternatives, const std::vector<std::string>& criteria);
+        Matrix2D getMatrix();
 
-    void addComparison(const std::string& alt1, const std::string& alt2, double val);
+    private:
+        const std::vector<std::string>& alternatives_;
+        const std::vector<std::string>& criteria_;
+        Comparisons comps_;
+        Matrix2D mat_;
+    };
 
-    Matrix2D getMatrix();
-
-  private:
-    const std::vector<std::string>& alternatives_;
-    const std::vector<std::string>& criteria_;
-    Comparisons comps_;
-    Matrix2D mat_;
-  };
-
-
-  class AHPRanker {
-    //TODO
-  };
+    class AHPRanker {
+    public:
+        AHPResult calculateRanking(Matrix2D criteria_comparison, std::vector<Matrix2D> alternatives_comparisons);
+        
+    private:
+        WeightsAndIR calculateWeightsAndIR(Matrix2D matrix);
+        std::vector<double> calculateRankingVector(Matrix2D matrix);
+    };
 
 }
